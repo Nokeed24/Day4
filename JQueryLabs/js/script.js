e9e1d60906ceaddd2794b04bdd2d4f08715f6485
Unknown <nokid_gr@hotmail.it>
2018-04-26 15:04:51 +0200

    Color change implemented

---
+++ JQueryLabs/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // Code here will run when the page has loaded.
+  $('input[type=radio][name=fontname]').change(function(e) {
+    $('#canvas').css('font-family', e.target.value);
+    console.log(e.target.value);
+  })
+
+  $('#textcolor').change(function(e) {
+    $('#canvas').css('color', e.target.value);
+  });
+
+
+
+});
